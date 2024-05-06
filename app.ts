61ea0ab3ad27af7cbbeddc68ae83dc6b5b738ad2
Hammad8288 <159915404+Hammad8288@users.noreply.github.com>
2024-05-06 14:51:59 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const answer :{
+    sentence: string
+} = await inquirer.prompt([
+        {
+            name : "sentence",
+            type : "input",
+            message : "Enter Your Sentence to Count Words"
+        }
+    ]);
+
+
+// Apply the Method of trim And Split
+const words = answer.sentence.trim().split(" ")
+
+// Print the array of words to the console 
+console.log(words);
+
+// Count The Length of Sentence
+console.log(`Your Sentence Word Count Is ${words.length}`);
+
+
